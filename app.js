728b40b74531333900e5d613025d42f7afaa7f4e
TaniiGo <keigo11235214@gmail.com>
2022-11-16 16:47:56 +0900

    renderの環境変数を利用できるように修正

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ User.sync().then(async () => {
 });
 
 const GitHubStrategy = require('passport-github2').Strategy;
-const GITHUB_CLIENT_ID = '2f831cb3d4aac02393aa';
-const GITHUB_CLIENT_SECRET = '9fbc340ac0175123695d2dedfbdf5a78df3b8067';
+const GITHUB_CLIENT_ID = '6a15ae83e708f9758329';
+const GITHUB_CLIENT_SECRET = 'fe2052ad8d3f44f4db35e19200812609a41e6e83';
 
 passport.serializeUser(function (user, done) {
   done(null, user);
@@ -39,7 +39,7 @@ passport.deserializeUser(function (obj, done) {
 passport.use(new GitHubStrategy({
   clientID: GITHUB_CLIENT_ID,
   clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: 'http://localhost:8000/auth/github/callback'
+  callbackURL: process.env.CALLBACK_URL || 'http://localhost:8000/auth/github/callback'
 },
   function (accessToken, refreshToken, profile, done) {
     process.nextTick(async function () {
